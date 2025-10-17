d653368604a51c6d3c1e83fcb20df889d9937573
Tali Auster <me@tali.network>
2025-10-16 19:27:56 -0700

    refactor(cli): use CachingEnvironmentT

--- app/Main.hs
+++ app/Main.hs
@@ -1,22 +1,30 @@
+{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE MonoLocalBinds #-}
+
 module Main where
 
+import Control.Monad.State.Lazy (evalStateT, get, lift)
 import Data.Cache.Sim.Algs (fitf, s3fifo)
-import Data.Cache.Sim.Types (set, stepper)
-import System.IO
+import Data.Cache.Sim.Types (CachingEnvironmentT, request)
+import System.IO (hFlush, stdout)
+
+main' :: (Show c) => CachingEnvironmentT String c IO ()
+main' = repl []
+  where
+    repl rqs = do
+      get >>= lift . print
+      rq <- lift getReq
+      act <- request rq
+      let rqs' = rqs ++ [rq]
+      lift $ print act
+      lift $ print $ fitf 2 rqs'
+      repl rqs'
+
+getReq :: IO String
+getReq = do
+  putStr "rq> "
+  _ <- hFlush stdout
+  getLine
 
 main :: IO ()
-main = main' [] $ s3fifo (2, 4)
-  where
-    main' rSeq alg = do
-      print alg
-      (rSeq', act, state') <- getReq rSeq alg
-      print act
-      print $ fitf 2 rSeq'
-      main' rSeq' $ set alg state'
-
-    getReq rSeq cache = do
-      putStr "rq> "
-      _ <- hFlush stdout
-      rq <- getLine
-      let (action, cache') = stepper cache rq
-      return (rSeq ++ [rq], action, cache')
+main = evalStateT main' $ s3fifo (2, 4)
