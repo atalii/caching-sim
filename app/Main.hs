78b5123cefae3aa50de38e139d5dfd36e7fae860
Tali Auster <me@tali.network>
2025-11-11 10:20:11 -0800

    GARBAGE CODE: try a bunch of S's, M's.

--- app/Main.hs
+++ app/Main.hs
@@ -1,37 +1,21 @@
 {-# LANGUAGE FlexibleContexts #-}
 {-# LANGUAGE MonoLocalBinds #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE TupleSections #-}
 
 module Main where
 
-import Control.Monad.State.Lazy (evalStateT, get, lift)
-import Data.Cache.Sim.Algs (fitf, s3fifo)
-import Data.Cache.Sim.Types (CachingEnvironmentT, request)
-import System.IO (hFlush, stdout)
-
-main' :: (Show c) => CachingEnvironmentT String c IO ()
-main' = repl []
-  where
-    repl rqs = do
-      lift $ putStr "[\x1b[33;1mA  \x1b[0m] "
-      get >>= lift . print
-
-      let (optSeq, cost) = fitf 6 rqs
-      lift $ putStr "[\x1b[32;1mOPT\x1b[0m] "
-      lift $ mapM_ (putStrLn . ('\t' :) . show) optSeq
-      lift $ print cost
-
-      rq <- lift getReq
-      act <- request rq
-      let rqs' = rqs ++ [rq]
-      lift $ print act
-
-      repl rqs'
-
-getReq :: IO String
-getReq = do
-  putStr "rq> "
-  _ <- hFlush stdout
-  getLine
+import Data.Cache.Sim.Algs (fitf)
+import Data.Cache.Sim.Attacks (s3fifoAttack)
 
 main :: IO ()
-main = evalStateT main' $ s3fifo (2, 4)
+main = mapM_ runOn configs
+  where
+    configs = concatMap genUpTo [1 .. 100]
+    l = 1000000
+    runOn conf@(s, m) =
+      let attack = take l $ s3fifoAttack conf
+          cost :: Double = fromIntegral l / fromIntegral (snd (fitf (s + m) attack))
+       in putStrLn $ show s <> "," <> show m <> "," <> show cost
+
+    genUpTo n = map (,n) [1 .. n]
